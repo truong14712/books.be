cba4880e11471dfc567db45e9aa87a5c43faf2b9
Minh Trường <letruong147123@gmail.com>
2023-09-23 10:50:10 +0700

    checkPermission and fix cloudinary

---
+++ src/middlewares/checkPermission.js
@@ -0,0 +1,59 @@
+/* eslint-disable no-console */
+import dotenv from 'dotenv';
+import jwt from 'jsonwebtoken';
+import responseStatus from '~/constants/responseStatus';
+import userModel from '~/models/userModel';
+import ErrorHandler from '~/utils/ErrorHandler';
+
+dotenv.config();
+
+const CheckMiddleware = {
+  async authentication(req, res, next) {
+    try {
+      const authHeader = req.headers.authorization;
+      if (!authHeader) return next(new ErrorHandler('Please login to continue', 401));
+      const token = authHeader && authHeader.split(' ')[1];
+      if (!token) {
+        return next(new ErrorHandler('Invalid token!', 401));
+      }
+      const { ACCESS_TOKEN_SECRET } = process.env;
+      jwt.verify(token, ACCESS_TOKEN_SECRET, async (error, payload) => {
+        if (error instanceof jwt.JsonWebTokenError) {
+          return res.status(400).json({
+            message: 'JsonWebTokenError',
+          });
+        }
+        if (error instanceof jwt.TokenExpiredError) {
+          return res.status(400).json({
+            message: 'TokenExpiredError',
+          });
+        }
+        const user = await userModel.findById(payload.id);
+        if (!user)
+          return next(new ErrorHandler(responseStatus.UNAUTHORIZED.message, responseStatus.UNAUTHORIZED.status));
+        req.user = user;
+        next();
+      });
+    } catch (error) {
+      if (error.name === 'TokenExpiredError') {
+        return res.status(200).json({ code: 401, message: error.message });
+      }
+      if (error.name === 'JsonWebTokenError') {
+        return res.status(200).json({ code: 401, message: error.message });
+      }
+      next(error);
+    }
+  },
+  async authorization(req, res, next) {
+    try {
+      const user = req.user;
+      if (!(user.role === 'admin')) {
+        return next(new ErrorHandler(`Role: ${req.user.role} is not allowed to access this resources `, 403));
+      }
+      next();
+    } catch (error) {
+      next(error);
+    }
+  },
+};
+export default CheckMiddleware;
--- src/middlewares/cloudinary.config.js
+++ src/middlewares/cloudinary.config.js
@@ -2,6 +2,7 @@ import { v2 as cloudinary } from 'cloudinary';
 import { CloudinaryStorage } from 'multer-storage-cloudinary';
 import multer from 'multer';
 import dotenv from 'dotenv';
+import sharp from 'sharp';
 dotenv.config();
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_NAME,
@@ -14,6 +15,16 @@ const storage = new CloudinaryStorage({
   allowedFormats: ['jpg', 'png'],
   params: {
     folder: 'books_online_backend',
+    resource_type: 'image',
+  },
+  transformer: async function (req, file, cb) {
+    const stream = await sharp({ sequentialRead: true, sequentialWrite: true })
+      .resize({ width: 500, fit: 'fill' })
+      .jpeg({ quality: 80 })
+      .rotate(90)
+      .png({ quality: 80 })
+      .toBuffer();
+    cb(null, stream);
   },
 });
 export { cloudinary };
